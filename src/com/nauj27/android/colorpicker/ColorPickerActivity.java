43aea1fd4e648948a02703b93b2d7d25e0be5afc
nauj27 <nauj27@ratonera.nauj27.com>
2010-03-02 09:03:12 +0100

    Borrado el de capitalizaci?n incorrecta

---
+++ src/com/nauj27/android/colorpicker/ColorPickerActivity.java
@@ -0,0 +1,204 @@
+package com.nauj27.android.colorpicker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+/**
+ * Main activity for color picker.
+ * @author nauj27
+ * Extends Activity
+ */
+public class ColorPickerActivity extends Activity {
+	// Private constants.
+	private static final String TAG = "ColorPickerActivity";
+	private static final int BITMAP_FROM_CAMERA = 0;
+	private static final int MENU_TAKE_PHOTO_ITEM = Menu.FIRST;	
+	
+	// Private variables.
+	private boolean photoTaken = false;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.color_picker_layout);
+        
+        ImageView imageView = (ImageView)findViewById(R.id.ivPicture);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        
+        imageView.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View view, MotionEvent motionEvent) {
+				Log.d(TAG, "Screen touched");
+				
+				int action = motionEvent.getAction();
+				
+				switch(action) {
+					case(MotionEvent.ACTION_DOWN):
+						int x = (int)motionEvent.getX();
+						int y = (int)motionEvent.getY();
+						Log.d(TAG, "Position: " + x + ", " + y);
+						if (photoTaken) {
+							findColor(view, x, y);
+						} else {
+					    	Context context = getApplicationContext();
+					    	CharSequence charSequence = "Haga una foto usando la tecla menu";
+					    	int duration = Toast.LENGTH_SHORT;
+					    	Toast toast = Toast.makeText(context, charSequence, duration);
+					    	toast.show();
+						}
+				}
+				return false;
+			}
+		});
+    }
+    
+    /**
+     * Find components of color of the bitmap at x, y. 
+     * @param x Distance from left border of the View
+     * @param y Distance from top of the View
+     * @param view Touched surface on screen
+     */
+    private void findColor(View view, int x, int y) {
+    	int red = 0;
+    	int green = 0;
+    	int blue = 0;
+    	int color = 0;
+    	
+    	int offset = 1; // 3x3 Matrix
+    	int pixelsNumber = 0;
+    	
+    	int xImage = 0;
+    	int yImage = 0;
+    	
+    	ImageView imageView = (ImageView)view;
+    	Log.d(TAG, "View size: " + imageView.getWidth() + "x" + imageView.getHeight());
+    	BitmapDrawable bitmapDrawable = (BitmapDrawable)imageView.getDrawable();
+    	Bitmap imageBitmap = bitmapDrawable.getBitmap();
+    	Log.d(TAG, "Bitmap size: " + imageBitmap.getWidth() + "x" + imageBitmap.getHeight());
+
+        // Calculate the target in the bitmap.
+    	xImage = (int)(x * ((double)imageBitmap.getWidth() / (double)imageView.getWidth()));
+    	yImage = (int)(y * ((double)imageBitmap.getHeight() / (double)imageView.getHeight()));
+        Log.d(TAG, "Transformation: " + x + "x" + y + " => " + xImage + "x" + yImage);
+    	
+        // Average of pixels around the touched one.
+    	for (int i = xImage - offset; i <= xImage + offset; i++) {
+    		for (int j = yImage - offset; j <= yImage + offset; j++) {
+    			try {
+        			color = imageBitmap.getPixel(i, j);
+        			red += Color.red(color);
+        			green += Color.green(color);
+        			blue += Color.blue(color);
+        			pixelsNumber += 1;
+        			Log.d(TAG, "Color del punto " + i + " :(" + 
+        					Color.red(color) + ", " + Color.green(color) + 
+        					", " + Color.blue(color) + ")");
+        		} catch(Exception e) {
+        			Log.w(TAG, "Error picking color!");
+        		}	
+    		}
+    	}
+    	red = red / pixelsNumber;
+    	green = green / pixelsNumber;
+    	blue = blue / pixelsNumber;
+    	
+    	CharSequence msg = "Color: (" + red + ", " + green + ", " + blue + ")"; 
+    	Log.d(TAG, (String)msg);
+    	
+    	Context context = getApplicationContext();
+    	int duration = Toast.LENGTH_SHORT;
+    	Toast toast = Toast.makeText(context, msg, duration);
+    	toast.show();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	
+    	int groupId = 0;
+    	int menuItemId = MENU_TAKE_PHOTO_ITEM;
+    	int menuItemOrder = Menu.NONE;
+    	int menuItemText = R.string.menu_take_photo_item;
+    	
+    	// Create the menu item and keep a reference to it.
+    	//MenuItem menuItem = menu.add(groupId, menuItemId, menuItemOrder, menuItemText);
+    	menu.add(groupId, menuItemId, menuItemOrder, menuItemText);
+    	//menuItem.setIcon(R.drawable.menu_item_icon);
+    	return true;
+    }
+    
+    @Override
+    /**
+     * What to do when a option menu is selected.
+     * @param menuItem Item selected from menu.
+     */
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+    	super.onOptionsItemSelected(menuItem);
+    	
+    	// Search for menu item.
+    	switch(menuItem.getItemId()) {
+	    	case(MENU_TAKE_PHOTO_ITEM):
+	    		Intent takePhotoIntent = new Intent(this, TakePhotoActivity.class);
+	    		startActivityForResult(takePhotoIntent, BITMAP_FROM_CAMERA);	    	
+	    		return true;
+    	}
+    	
+    	// Menu item does not exist.
+    	return false;
+    }
+    
+    @Override
+    /**
+     * Overrides actions on activity result from other activity.
+     * @param requestCode The request code
+     * @param resultCode The result code
+     * @param data Data returned from activity
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	super.onActivityResult(requestCode, resultCode, data);
+    	
+    	final String JPEG_PICTURE = "JPEG_PICTURE";
+    	
+    	switch(requestCode) {
+    	case BITMAP_FROM_CAMERA:
+    		if (resultCode == RESULT_OK) {
+    			
+    			Bundle bundle = data.getExtras();
+    			if (bundle.containsKey(JPEG_PICTURE)) {
+    				
+    				byte[] jpegPicture = bundle.getByteArray(JPEG_PICTURE);    			
+    			
+	    			int offset = 0;
+	    			int length = jpegPicture.length;
+	    			Bitmap bitmap = BitmapFactory
+	    				.decodeByteArray(jpegPicture, offset, length);
+	    			
+	    			ImageView imageView = (ImageView)findViewById(R.id.ivPicture);
+	    			imageView.setImageBitmap(bitmap);
+	    			
+	    			photoTaken = true;
+	    			
+    			} else {
+    				// pass
+    			}
+    			
+    		}
+    		break;
+    	}
+    }
+}
---
+++ src/com/nauj27/android/colorpicker/TakePhotoActivity.java
@@ -0,0 +1,134 @@
+/**
+ * Activity to make photo.
+ */
+package com.nauj27.android.colorpicker;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.AutoFocusCallback;
+import android.hardware.Camera.PictureCallback;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * @author nauj27
+ *
+ */
+public class TakePhotoActivity extends Activity {
+	private static final String TAG = "TakePhotoActivity";
+	private Camera camera = null;
+	
+	private static final int PICTURE_SIZE_WIDTH = 480;
+	private static final int PICTURE_SIZE_HEIGHT = 640;
+	private static final int PREVIEW_SIZE_WIDTH = 640;
+	private static final int PREVIEW_SIZE_HEIGHT = 480;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		
+		getWindow().setFormat(PixelFormat.TRANSLUCENT);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		setContentView(R.layout.take_photo_layout);
+		
+		SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surfaceViewCamera);
+		surfaceView.setOnClickListener(onClickListener);
+		
+		SurfaceHolder surfaceHolder = surfaceView.getHolder();
+		surfaceHolder.addCallback(surfaceCallback);
+		surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+				
+	}
+	
+	private View.OnClickListener onClickListener = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			camera.autoFocus(autoFocusCallback);
+		}
+
+	};
+
+	
+	private AutoFocusCallback autoFocusCallback = new AutoFocusCallback() {
+		@Override
+		public void onAutoFocus(boolean arg0, Camera camera) {
+	        camera.takePicture(null, null, jpegCallback);			
+		}
+	};
+	
+	
+	private PictureCallback jpegCallback = new PictureCallback() {
+
+		@Override
+		public void onPictureTaken(byte[] jpegPicture, Camera camera) {
+			final String JPEG_PICTURE = "JPEG_PICTURE";
+			
+			Intent photoIntent = new Intent();			
+			photoIntent.putExtra(JPEG_PICTURE , jpegPicture);
+			setResult(RESULT_OK, photoIntent);
+			
+			finish();
+		}
+	};
+	
+	
+	private SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+		@Override
+		public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+			camera.stopPreview();
+			camera.release();
+			camera = null;
+		}
+
+		@Override
+		public void surfaceChanged(
+				SurfaceHolder surfaceHolder, int format, int width, int height) {
+			
+			Log.d(TAG, "SurfaceHolder size: "
+					.concat(new Integer(width).toString())
+					.concat("x")
+					.concat(new Integer(height).toString()));
+			
+			Camera.Parameters cameraParameters = camera.getParameters();
+			
+			cameraParameters.setPictureSize(PICTURE_SIZE_WIDTH, PICTURE_SIZE_HEIGHT);
+			cameraParameters.setPreviewSize(PREVIEW_SIZE_WIDTH, PREVIEW_SIZE_HEIGHT);
+			cameraParameters.setPictureFormat(PixelFormat.JPEG);
+			
+			// FIXME: Esto solo funciona con el teléfono en vertical.
+			// Al menos en el HTC Hero. Si no se pone se tuerce la imagen.
+			// En el magic se tuerce la vista previa!!
+			//cameraParameters.set("rotation", 90);
+
+			camera.setParameters(cameraParameters);
+			camera.startPreview();
+		}
+
+		@Override
+		public void surfaceCreated(SurfaceHolder surfaceHolder) {
+			camera = Camera.open();
+			
+			try {
+				camera.setPreviewDisplay(surfaceHolder);
+			} catch (IOException ioException) {
+				camera.release();
+				camera = null;
+			}
+		}
+	};
+}
