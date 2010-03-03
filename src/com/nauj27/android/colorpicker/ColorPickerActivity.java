162bcfe8864bd33c78c09cc39d3c4d6477311967
nauj27 <nauj27@arriba.nauj27.com>
2010-03-03 19:18:59 +0100

    Borrada clase BitmapFromUri que no se usaba. Ahora se ha sacado la parte de b?squeda del color a la clase nueva Utils.

--- src/com/nauj27/android/colorpicker/ColorPickerActivity.java
+++ src/com/nauj27/android/colorpicker/ColorPickerActivity.java
@@ -8,7 +8,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -72,7 +71,16 @@ public class ColorPickerActivity extends Activity {
 						int y = (int)motionEvent.getY();
 						Log.d(TAG, "Position: " + x + ", " + y);
 						if (photoTaken) {
-							findColor(view, x, y);
+							int color = Utils.findColor(view, x, y);
+							
+							CharSequence msg = "Color: (" 
+								+ Color.red(color) + ", "
+								+ Color.green(color) + ", "
+								+ Color.blue(color) + ")";
+							Context context = getApplicationContext();
+					    	int duration = Toast.LENGTH_SHORT;
+					    	Toast toast = Toast.makeText(context, msg, duration);
+					    	toast.show();
 						} else {
 					    	Context context = getApplicationContext();
 					    	CharSequence charSequence = getString(R.string.help_message);
@@ -84,65 +92,6 @@ public class ColorPickerActivity extends Activity {
 				return false;
 			}
 		});
-    }
-    
-    /**
-     * Find components of color of the bitmap at x, y. 
-     * @param x Distance from left border of the View
-     * @param y Distance from top of the View
-     * @param view Touched surface on screen
-     */
-    private void findColor(View view, int x, int y) {
-    	int red = 0;
-    	int green = 0;
-    	int blue = 0;
-    	int color = 0;
-    	
-    	int offset = 1; // 3x3 Matrix
-    	int pixelsNumber = 0;
-    	
-    	int xImage = 0;
-    	int yImage = 0;
-    	
-    	ImageView imageView = (ImageView)view;
-    	Log.d(TAG, "View size: " + imageView.getWidth() + "x" + imageView.getHeight());
-    	BitmapDrawable bitmapDrawable = (BitmapDrawable)imageView.getDrawable();
-    	Bitmap imageBitmap = bitmapDrawable.getBitmap();
-    	Log.d(TAG, "Bitmap size: " + imageBitmap.getWidth() + "x" + imageBitmap.getHeight());
-
-        // Calculate the target in the bitmap.
-    	xImage = (int)(x * ((double)imageBitmap.getWidth() / (double)imageView.getWidth()));
-    	yImage = (int)(y * ((double)imageBitmap.getHeight() / (double)imageView.getHeight()));
-        Log.d(TAG, "Transformation: " + x + "x" + y + " => " + xImage + "x" + yImage);
-    	
-        // Average of pixels around the touched one.
-    	for (int i = xImage - offset; i <= xImage + offset; i++) {
-    		for (int j = yImage - offset; j <= yImage + offset; j++) {
-    			try {
-        			color = imageBitmap.getPixel(i, j);
-        			red += Color.red(color);
-        			green += Color.green(color);
-        			blue += Color.blue(color);
-        			pixelsNumber += 1;
-        			Log.d(TAG, "Color del punto " + i + " :(" + 
-        					Color.red(color) + ", " + Color.green(color) + 
-        					", " + Color.blue(color) + ")");
-        		} catch(Exception e) {
-        			Log.w(TAG, "Error picking color!");
-        		}	
-    		}
-    	}
-    	red = red / pixelsNumber;
-    	green = green / pixelsNumber;
-    	blue = blue / pixelsNumber;
-    	
-    	CharSequence msg = "Color: (" + red + ", " + green + ", " + blue + ")"; 
-    	Log.d(TAG, (String)msg);
-    	
-    	Context context = getApplicationContext();
-    	int duration = Toast.LENGTH_SHORT;
-    	Toast toast = Toast.makeText(context, msg, duration);
-    	toast.show();
     }
     
     @Override
