3fa59c5269e33a8e11aff1b6469e6eb71bef64ce
nauj27 <nauj27@arriba.nauj27.com>
2010-04-06 20:39:24 +0200

    Icono para pantalla de foto sobre imagen. Lo l?gico ser?a que ese activity fuese el principal.

--- src/com/nauj27/android/colorpicker/TakePhotoActivity.java
+++ src/com/nauj27/android/colorpicker/TakePhotoActivity.java
@@ -30,8 +30,9 @@ public class TakePhotoActivity extends Activity {
 	private static final String TAG = "TakePhotoActivity";
 	private Camera camera = null;
 	
-	private static final int PICTURE_SIZE_WIDTH = 352;
-	private static final int PICTURE_SIZE_HEIGHT = 288;
+	// FIXME: size is not standard, but only can use getValid in 2.1 :(
+	private static final int PICTURE_SIZE_WIDTH = 512; //352;
+	private static final int PICTURE_SIZE_HEIGHT = 384; //288;
 	private static final int PREVIEW_SIZE_WIDTH = 352;
 	private static final int PREVIEW_SIZE_HEIGHT = 288;
 	
