7e0a8c33448e9a2f585d9487c65262ea9ca4c846
nauj27 <nauj27@arriba.nauj27.com>
2010-04-24 14:15:41 +0200

    Flujo del programa correcto. El color se informa con un toast y quiero cambiarlo por un dialog m?s completo y controlado.

--- src/com/nauj27/android/colorpicker/TakePhotoActivity.java
+++ src/com/nauj27/android/colorpicker/TakePhotoActivity.java
@@ -14,6 +14,8 @@ import android.hardware.Camera.AutoFocusCallback;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -28,6 +30,9 @@ import android.widget.ImageButton;
  */
 public class TakePhotoActivity extends Activity {
 	private static final String TAG = "TakePhotoActivity";
+	private static final int MENU_TAKE_PHOTO_ITEM = Menu.FIRST;
+	private static final int MENU_TAKE_PHOTO_EXIT = 10; // The last one.
+	
 	private Camera camera = null;
 	
 	// FIXME: size is not standard, but only can use getValid in 2.1 :(
@@ -46,7 +51,7 @@ public class TakePhotoActivity extends Activity {
     	// This is the only way camera preview work on all android devices
 		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		
-		// No title, no name: Fullscreen
+		// No title, no name: Full screen
 		getWindow().setFormat(PixelFormat.TRANSLUCENT);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -64,6 +69,9 @@ public class TakePhotoActivity extends Activity {
 				
 	}
 	
+	/**
+	 * Create the on click listener for the button.
+	 */
 	private View.OnClickListener onClickListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
@@ -79,7 +87,10 @@ public class TakePhotoActivity extends Activity {
 		}
 	};
 	
-	
+	/**
+	 * JPEG data is available after take picture.
+	 * Put data into extras of Intent and launch new intent for ColorPicker.
+	 */
 	private PictureCallback jpegCallback = new PictureCallback() {
 		@Override
 		public void onPictureTaken(byte[] jpegPicture, Camera camera) {
@@ -91,7 +102,9 @@ public class TakePhotoActivity extends Activity {
 		}
 	};
 	
-	
+	/**
+	 * Create the surface callback for 
+	 */
 	private SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
 		@Override
 		public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
@@ -127,4 +140,56 @@ public class TakePhotoActivity extends Activity {
 			}
 		}
 	};
+	
+    @Override
+    /**
+     * Add items to the menu for this activity.
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	
+    	final int groupId = 0;
+    	int menuItemId;
+    	int menuItemOrder;
+    	int menuItemText;
+    	MenuItem menuItem;
+
+    	// Button to take picture
+    	menuItemId = MENU_TAKE_PHOTO_ITEM;
+    	menuItemOrder = MENU_TAKE_PHOTO_ITEM;
+    	menuItemText = R.string.menu_take_photo_item;
+    	menuItem = menu.add(groupId, menuItemId, menuItemOrder, menuItemText);
+    	menuItem.setIcon(android.R.drawable.ic_menu_camera);
+    	
+    	// Button to exit the application
+    	menuItemId = MENU_TAKE_PHOTO_EXIT;
+    	menuItemOrder = MENU_TAKE_PHOTO_EXIT;
+    	menuItemText = R.string.menu_take_photo_exit;
+    	menuItem = menu.add(groupId, menuItemId, menuItemOrder, menuItemText);
+    	menuItem.setIcon(android.R.drawable.ic_menu_close_clear_cancel);
+    	
+    	return true;
+    }
+    
+    @Override
+    /**
+     * What to do when a option menu is selected.
+     * @param menuItem Item selected from menu.
+     */
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+    	super.onOptionsItemSelected(menuItem);
+    	
+    	// Search for menu item.
+    	switch(menuItem.getItemId()) {
+	    	case(MENU_TAKE_PHOTO_ITEM):
+	    		camera.autoFocus(autoFocusCallback);	    	
+	    		return true;
+	    	case(MENU_TAKE_PHOTO_EXIT):
+	    		finish();
+	    		return true;
+    	}
+    	
+    	// Menu item does not exist.
+    	return false;
+    }
 }
