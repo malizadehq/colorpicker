34e30aebac9cdfaa2fc88bc4d8d5c4e32ac80873
nauj27 <nauj27@arriba.nauj27.com>
2010-04-26 18:38:59 +0200

    Creando di?logo de resutlado de color

--- gen/com/nauj27/android/colorpicker/R.java
+++ gen/com/nauj27/android/colorpicker/R.java
@@ -15,13 +15,22 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int ImageCameraButton=0x7f050002;
+        public static final int ImageCameraButton=0x7f05000a;
+        public static final int ImageViewColor=0x7f050004;
+        public static final int LinearLayoutColor=0x7f050003;
+        public static final int LinearLayoutData=0x7f050005;
+        public static final int TableLayout01=0x7f050001;
+        public static final int TextViewHex=0x7f050008;
+        public static final int TextViewHumanReadable=0x7f050002;
+        public static final int TextViewRal=0x7f050006;
+        public static final int TextViewRgb=0x7f050007;
         public static final int ivPicture=0x7f050000;
-        public static final int surfaceViewCamera=0x7f050001;
+        public static final int surfaceViewCamera=0x7f050009;
     }
     public static final class layout {
         public static final int color_picker_layout=0x7f030000;
-        public static final int take_photo_layout=0x7f030001;
+        public static final int result_layout=0x7f030001;
+        public static final int take_photo_layout=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
@@ -30,6 +39,10 @@ public final class R {
         public static final int make_photo=0x7f040000;
         public static final int menu_take_photo_exit=0x7f040006;
         public static final int menu_take_photo_item=0x7f040002;
+        public static final int result_color_hex=0x7f04000a;
+        public static final int result_color_name=0x7f040007;
+        public static final int result_color_ral=0x7f040008;
+        public static final int result_color_rgb=0x7f040009;
         public static final int take_photo_help=0x7f040004;
     }
 }
