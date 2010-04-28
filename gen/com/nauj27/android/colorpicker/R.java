89a8f078ea667dd21eca1aedca3c4ddbb7663dbd
nauj27 <nauj27@arriba.nauj27.com>
2010-04-28 18:44:44 +0200

    Bot?n de hacer foto ahora tiene feedback visual

--- gen/com/nauj27/android/colorpicker/R.java
+++ gen/com/nauj27/android/colorpicker/R.java
@@ -11,8 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int btn_camera=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int button_camera=0x7f020000;
+        public static final int button_camera_focused=0x7f020001;
+        public static final int button_camera_normal=0x7f020002;
+        public static final int button_camera_pressed=0x7f020003;
+        public static final int icon=0x7f020004;
     }
     public static final class id {
         public static final int ImageCameraButton=0x7f050009;
